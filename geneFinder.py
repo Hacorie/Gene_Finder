cbba98e2164a0b57338abd1ddb766d8e45e3fdb3
Nathan Perry <hacorie@gmail.com>
2013-11-21 17:28:35 -0600

    Added More Comments
    Added a bit of comments to explain the process of finding a potential
    gene that I am using.

--- geneFinder.py
+++ geneFinder.py
@@ -15,38 +15,48 @@ def findPotentialGenes(pos, neg):
 
 def geneSearch(strand):
 	"""Takes a positive or negative strand and return potential genes"""
-	genes = []
+	genes = []	#hold all potential genes found
 	for frameNo in range(0,3):
-		num = 0 #number of genes found on this strand
+		#num = 0 #number of genes found on this strand
 
 		#loop over every contig in the strand
 		for key,value in strand.items():
-			#record the starting pos of the strand
-			startPos = frameNo; #starting position of potential gene
+			#start posisition of current strand data in correct reading frame
+			startPos = frameNo;
 
+			#loop over entire contig data
 			while(startPos < len(value)-3):
-				codon = value[startPos:startPos+3]
-				if(codon == "atg"):
-					gene = []
-					geneStart = nextCodon = startPos
 
+				#pull out individual codon
+				codon = value[startPos:startPos+3]
+
+				#only accept atg as start codon for a gene
+				if(codon == "atg"):
+					gene = []	#holds potential gene
+
+					#record where potential gene starts
+					geneStart = nextCodon = startPos 
+
+					#make sure not to exceed length of contig data
 					while geneStart < len(value)-2:
+						#update the current Codon
 						currCodon = value[nextCodon:nextCodon+3]
 
+						#accept "taa" "tag" and "tga" as the only possible gene stop codons
 						if(currCodon == "taa") or currCodon == "tag" or currCodon == "tga":
 							gene.append(currCodon)
-							break;
-						else:
+							break; #potential gene found!
+						else: #keep going if currCodon is not one of above
 							gene.append(currCodon)
 							nextCodon += 3
 
+					#set some "bounds" on the possible length of a gene
 					if(len(gene)*3 >= 99 and len(gene)*3 <= 1500):
-						contigid = key
-						length = len(gene)*3
-						genes.append(GeneInfo(length, contigid, geneStart))
-						num += 1
-						startPos += length
+						genes.append(GeneInfo(len(gene)*3, key, geneStart))
+						#num += 1
+						startPos += length #assume one gene is not inside another gene
 						continue
+				#update position of next codon
 				startPos += 3
 
 		return genes
